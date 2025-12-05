00c35f6165bd8bc46d5c6059d06ae9648a199d3a
muhammad bunyamin <muhammad.bunyamin@radenfatah.ac.id>
2025-12-05 16:17:44 +0700

    add tugas

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -18,9 +18,49 @@ class PostController extends Controller
         return $user;
     }
 
+    
+    #tugas
     public function CatagoryPosts()
     {
-        $catagory = Catagory::where('slug', 'technology');
+        $catagory = Catagory::where('slug', 'technology')->first();
         return $category;
+
+        /*
+        {
+            "id": 1,
+            "slug": "technology",
+            "category": "Technology",
+            "posts": [
+                {
+                    "id": 5,
+                    "title": "The Future of AI: Trends to Watch in 2025",
+                    "feature_image": {
+                        "id": 5,
+                        "post_id": 5,
+                        "feature_image": "feature_images/ai_future.jpg"
+                    },
+                    user: {
+                        "id": 2,
+                        "email": "a@a.com"
+                        "name": "User A"
+                    }
+                },
+                {
+                    "id": 12,
+                    "title": "Top 10 Programming Languages to Learn in 2025",
+                    "feature_image": {
+                        "id": 12,
+                        "post_id": 12,
+                        "feature_image": "feature_images/top_languages.jpg"
+                    },
+                    user: {
+                        "id": 2,
+                        "email": "a@a.com"
+                        "name": "User A"
+                    }
+                }
+            ]
+        }
+        */
     }
 }
